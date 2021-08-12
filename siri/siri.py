8b1fbec161c07609d901ca2490f8319d0fcdb839
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-08-12 11:52:18 +0530

    Update siri.py

--- siri/siri.py
+++ siri/siri.py
@@ -64,7 +64,7 @@ def a(client, message):
             #     m.edit("Exceeded 30mins cap")
             #     return
 
-            performer = f"[Nazriya\jinn 🇮🇳]" 
+            performer = f"[NAZRIYA 🇮🇳]" 
             thumb_name = f'thumb{message.message_id}.jpg'
             thumb = requests.get(thumbnail, allow_redirects=True)
             open(thumb_name, 'wb').write(thumb.content)
